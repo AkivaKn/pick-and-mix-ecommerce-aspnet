f31b66fd43422185b3e3e0c39fab45c6599fdd54
akivakn <akivakaufman@gmail.com>
2024-06-03 17:19:44 +0100

    feat: create Sweet model

---
+++ PickAndMixECommerce/Models/Sweet.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PickAndMixECommerce.Models
+{
+    public class Sweet
+    {
+        [Key]
+        public int SweetId { get; set; }
+        public string SweetName { get; set; }
+        public string BrandtName { get; set; }
+        public int PriceInPence { get; set; }
+        public string ImageUrl { get; set; }
+        public int StockInGrams { get; set; }
+
+    }
+}
