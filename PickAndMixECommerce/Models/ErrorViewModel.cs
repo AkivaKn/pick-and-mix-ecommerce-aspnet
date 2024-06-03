e017a659949686de60fb1b62ce7c7699826abf31
akiva <akiva@LAPTOP-DDJ002H7>
2024-06-03 09:42:43 +0100

    Add project files.

---
+++ PickAndMixECommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PickAndMixECommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
