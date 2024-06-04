0888c3008466a065318182abd46e6a2db35227f7
akivakn <akivakaufman@gmail.com>
2024-06-04 11:22:01 +0100

    feat: add Sweets view

---
+++ PickAndMixECommerce/Controllers/BrandsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PickAndMixECommerce.Data;
+
+namespace PickAndMixECommerce.Controllers
+{
+    public class BrandsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BrandsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var brandsData = await _context.Brands.ToListAsync();
+            return View(brandsData);
+        }
+    }
+}
---
+++ PickAndMixECommerce/Controllers/SweetsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PickAndMixECommerce.Data;
+
+namespace PickAndMixECommerce.Controllers
+{
+    public class SweetsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SweetsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var sweetsData = await _context.Sweets.ToListAsync();
+            return View(sweetsData);
+        }
+    }
+}
